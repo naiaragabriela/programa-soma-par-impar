da7de98751719086aceffbc5d7ea1797a36d8ab4
naiaragabriela <naigabi31@gmail.com>
2023-03-05 22:56:16 -0300

    soma de numeros pares e impares usando função

---
+++ src/ProgramaSomaParImpar/ProgramaSomaParImpar/Program.cs
@@ -0,0 +1,32 @@
+﻿int numero, somaPares, somaImpares;
+somaPares = 0;
+somaImpares = 0;
+
+
+int NumeroDigitado()
+{
+
+    Console.WriteLine("Digite um número inteiro");
+    return int.Parse(Console.ReadLine());
+}
+
+do
+{
+    
+    numero = NumeroDigitado();
+
+    if((numero<1000) && (numero > 0))
+    {
+        if (numero % 2 == 0)
+        {
+            somaPares += numero;
+        }
+        else
+        {
+            somaImpares += numero;
+        }
+    }
+} while (numero < 1000);
+
+Console.WriteLine($"A soma dos números pares digitados é: {somaPares}");
+Console.WriteLine($"A soma dos números ímpares digitados é: {somaImpares}");
